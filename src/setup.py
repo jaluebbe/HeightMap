2ae724b65796dfae3625276f99cf396f058b1f00
jannis <jluebbe@uos.de>
2019-09-08 13:14:16 +0200

    added Cython compiler directives for Python3.

--- src/setup.py
+++ src/setup.py
@@ -4,5 +4,6 @@ from Cython.Build import cythonize
 # all .pyx files in a folder
 setup(
   name = 'height_map',
-  ext_modules = cythonize(["*.pyx"]),
+  ext_modules = cythonize(["*.pyx"],
+      compiler_directives={'language_level' : "3"}),
 )
