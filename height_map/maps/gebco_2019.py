3b6d3d1966b6be34e0882031d472af5d1493fa0e
jannis <jluebbe@uos.de>
2019-09-08 23:26:12 +0200

    removed old min/max height methods from gebco_2019.py.

--- height_map/maps/gebco_2019.py
+++ height_map/maps/gebco_2019.py
@@ -105,6 +105,8 @@ def get_max_height_from_cache(i_ll, j_ll, i_ur, j_ur):
             counter = len(x)
             for _index in range(counter):
                 location_max += [(i_ll+x[_index], j_ll+y[_index])]
+    elif not os.path.isfile(cache_file):
+        print('GEBCO_2019 min/max cache file is missing.')
     return (location_max, round(float(h_max), 1), counter)
 
 def get_max_height_from_h5file(i_ll, j_ll, i_ur, j_ur):
@@ -137,34 +139,6 @@ def get_max_height(lat_ll, lon_ll, lat_ur, lon_ur):
         i_ll, j_ll, i_ur, j_ur)
     location_max = [[get_lat_from_index(_x), get_lon_from_index(_y)]
         for (_x, _y) in location_max]
-    return (location_max, round(float(h_max), 1), counter)
-
-# this method remains here for comparison until the cached_search is fully
-# implemented
-def get_max_height_old(lat_ll, lon_ll, lat_ur, lon_ur):
-    h_max = NODATA
-    location_max = []
-    counter = 0
-    if lon_ur >= 180 - CELLSIZE/2:
-        lon_ur -= CELLSIZE
-    # consider only correctly defined rectangle:
-    if ((lat_ll > lat_ur) or (lon_ll > lon_ur)):
-        return (location_max, NODATA, counter)
-    # convert coordinates to data indices:
-    i_ll = get_index_from_latitude(lat_ll)
-    j_ll = get_index_from_longitude(lon_ll)
-    i_ur = get_index_from_latitude(lat_ur)
-    j_ur = get_index_from_longitude(lon_ur)
-    file = os.path.join(path, filename)
-    if os.path.isfile(file):
-        with h5py.File(file, 'r') as f:
-            selection = f['elevation'][i_ll:i_ur+1, j_ll:j_ur+1]
-            h_max = selection.max()
-            x, y = np.where(selection==h_max)
-            counter = len(x)
-            for _index in range(counter):
-                location_max += [(get_lat_from_index(i_ll+x[_index]),
-                    get_lon_from_index(j_ll+y[_index]))]
     return (location_max, round(float(h_max), 1), counter)
 
 def get_min_height_from_indices(i_ll, j_ll, i_ur, j_ur):
@@ -228,6 +202,8 @@ def get_min_height_from_cache(i_ll, j_ll, i_ur, j_ur):
             counter = len(x)
             for _index in range(counter):
                 location_min += [(i_ll+x[_index], j_ll+y[_index])]
+    elif not os.path.isfile(cache_file):
+        print('GEBCO_2019 min/max cache file is missing.')
     return (location_min, round(float(h_min), 1), counter)
 
 def get_min_height_from_h5file(i_ll, j_ll, i_ur, j_ur):
@@ -260,36 +236,6 @@ def get_min_height(lat_ll, lon_ll, lat_ur, lon_ur):
         i_ll, j_ll, i_ur, j_ur)
     location_min = [[get_lat_from_index(_x), get_lon_from_index(_y)]
         for (_x, _y) in location_min]
-    if h_min == -NODATA:
-        h_min = NODATA
-    return (location_min, round(float(h_min), 1), counter)
-
-# this method remains here for comparison until the cached_search is fully
-# implemented
-def get_min_height_old(lat_ll, lon_ll, lat_ur, lon_ur):
-    location_min = []
-    counter = 0
-    if lon_ur >= 180 - CELLSIZE/2:
-        lon_ur -= CELLSIZE
-    # consider only correctly defined rectangle:
-    if ((lat_ll > lat_ur) or (lon_ll > lon_ur)):
-        return (location_min, NODATA, counter)
-    # convert coordinates to data indices:
-    i_ll = get_index_from_latitude(lat_ll)
-    j_ll = get_index_from_longitude(lon_ll)
-    i_ur = get_index_from_latitude(lat_ur)
-    j_ur = get_index_from_longitude(lon_ur)
-    h_min = -NODATA
-    file = os.path.join(path, filename)
-    if os.path.isfile(file):
-        with h5py.File(file, 'r') as f:
-            selection = f['elevation'][i_ll:i_ur+1, j_ll:j_ur+1]
-            h_min = selection.min()
-            x, y = np.where(selection==h_min)
-            counter = len(x)
-            for _index in range(counter):
-                location_min += [(get_lat_from_index(i_ll+x[_index]),
-                    get_lon_from_index(j_ll+y[_index]))]
     if h_min == -NODATA:
         h_min = NODATA
     return (location_min, round(float(h_min), 1), counter)
