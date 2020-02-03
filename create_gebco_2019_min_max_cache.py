9b461d3234ce32b1a647e7827790b5420b24470c
jannis <jluebbe@uos.de>
2020-02-03 21:10:12 +0100

    using the same file names as defined in gebco_2019.py .

--- create_gebco_2019_min_max_cache.py
+++ create_gebco_2019_min_max_cache.py
@@ -2,9 +2,10 @@ import h5py
 import os
 import json
 import numpy as np
-import height_map.maps.gebco_2019 as gebco_2019
+from height_map.gebco_2019 import Gebco2019
+gebco_2019 = Gebco2019()
 
-cache_filename = 'gebco_2019_cache.json'
+cache_filename = gebco_2019.cache_filename
 
 z_max = np.zeros([180, 360])
 z_min = np.zeros([180, 360])
@@ -20,4 +21,4 @@ for _x in range(180):
 min_max_cache = {'minimum': np.around(z_min, 1).tolist(),
     'maximum': np.around(z_max, 1).tolist()}
 with open(os.path.join(gebco_2019.cache_path, cache_filename), 'w') as f:
-    json.dump(cache, f)
+    json.dump(min_max_cache, f)
