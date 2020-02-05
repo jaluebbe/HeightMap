f5919d39d66cd1b3b595faab188d82688f1aa04a
jannis <jluebbe@uos.de>
2020-02-05 17:47:40 +0100

    modified create_gebco_2019_min_max_cache.py to use the Gebco2019 class.

--- create_gebco_2019_min_max_cache.py
+++ create_gebco_2019_min_max_cache.py
@@ -1,16 +1,14 @@
-import h5py
 import os
 import json
 import numpy as np
-import height_map.maps.gebco_2019 as gebco_2019
+from height_map.gebco_2019 import Gebco2019
 
-cache_filename = 'gebco_2019_cache.json'
+gebco = Gebco2019()
 
 z_max = np.zeros([180, 360])
 z_min = np.zeros([180, 360])
 
-with h5py.File(os.path.join(gebco_2019.path, gebco_2019.filename), 'r') as f:
-    data = np.array(f['elevation'])
+data = np.array(gebco.h5_file['elevation'])
 
 for _x in range(180):
     for _y in range(360):
@@ -19,5 +17,5 @@ for _x in range(180):
 
 min_max_cache = {'minimum': np.around(z_min, 1).tolist(),
     'maximum': np.around(z_max, 1).tolist()}
-with open(os.path.join(gebco_2019.cache_path, cache_filename), 'w') as f:
-    json.dump(cache, f)
+with open(os.path.join(gebco.cache_path, gebco.cache_file_name), 'w') as f:
+    json.dump(min_max_cache, f)
