851886c5aa6499b5f168aec244e3ee22b536ccc8
Jannis Lübbe <jannis@jl82.de>
2023-05-26 16:52:00 +0200

    added workaround for situations where ellipsoidalVincenty throws an exception.

--- height_map/__init__.py
+++ height_map/__init__.py
@@ -1,9 +1,16 @@
-import pygeodesy.ellipsoidalVincenty as eV
+import pygeodesy.ellipsoidalVincenty as ev
+import pygeodesy.ellipsoidalExact as ee
 
 
-def calculate_distance(lat1, lon1, lat2, lon2): 
+def calculate_distance(lat1, lon1, lat2, lon2):
     if lat1 == lat2 and lon1 == lon2:
         return 0
-    location_1 = eV.LatLon(lat1, lon1)
-    location_2 = eV.LatLon(lat2, lon2)
-    return location_1.distanceTo(location_2)
+    try:
+        location_1 = ev.LatLon(lat1, lon1)
+        location_2 = ev.LatLon(lat2, lon2)
+        distance = location_1.distanceTo(location_2)
+    except ev.VincentyError:
+        location_1 = ee.LatLon(lat1, lon1)
+        location_2 = ee.LatLon(lat2, lon2)
+        distance = location_1.distanceTo(location_2)
+    return distance
